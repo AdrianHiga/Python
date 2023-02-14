3e6a44ee70a3ef4bb9a2b8d7a9a044390a3ce905
AdrianHiga <102603609+AdrianHiga@users.noreply.github.com>
2023-02-14 12:26:35 -0300

    exercicios

---
+++ exercicio2.py
@@ -0,0 +1,3 @@
+nome = input('Qual o seu nome? ')
+
+print(f'É um prazer te conhecer, {nome}. ')
---
+++ exercicio3.py
@@ -0,0 +1,6 @@
+valor1 = input('Digite um valor: ')
+valor2 = input('Digite outro valor: ')
+valor1 = int(valor1)
+valor2 = int(valor2)
+soma = valor1 + valor2
+print(f'A soma entre {valor1} + {valor2} é igual a {soma}!')
